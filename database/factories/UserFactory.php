141374c7b0aef56fee5c07022ceb66d17edae741
Waqar Ali <mu.waqar.ali@gmail.com>
2022-03-06 01:34:53 +0500

    fix: admin user uuid

--- database/factories/UserFactory.php
+++ database/factories/UserFactory.php
@@ -20,6 +20,7 @@ class UserFactory extends Factory
         return [
             'first_name' => 'admin',
             'last_name' => 'admin',
+            'uuid' => Str::uuid(),
             'email' => 'admin@buckhill.co.uk',
             'email_verified_at' => now(),
             'password' => '$2y$10$92IXUNpkjO0rOQ5byMi.Ye4oKoEa3Ro9llC/.og/at2.uheWG/igi', // password
